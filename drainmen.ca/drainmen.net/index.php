ad7ac900cc0d0cb30c331ed8c2c575c67553d887
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:34:29 -0800

    Create index.php

---
+++ drainmen.ca/drainmen.net/index.php
@@ -0,0 +1,40 @@
+<?php include("header.php"); ?>
+
+<!------------------------ Page Content --------------------------------------------------->
+	
+	
+	<!--
+	<div id="page-content-header">
+	  <section class="header" id="page-header-title">
+	    <div class="container">
+		<h5 class="page-title">What's your favourite colour ?</h4>
+	    </div>
+	  </section>
+	</div>
+	-->
+	
+	<!--
+	<div id="homepage-banner" style="overflow: hidden; background: #fff;">
+		<img id="banner-img" src="resources/images/paintbuckets.jpg" style="opacity: 0.2" alt="homepage-banner" align="middle"/>
+	</div> <!-- homepage-banner -->
+	
+
+	<div id="page-content" class="homepage-background" style="padding-bottom:20px;">
+	<div id="page-content-margin"><br/></div>	
+	
+	
+	
+
+	
+	</div> <!-- end of page-content -->
+	
+	<!-- -->
+	<script language="javascript">
+	var ht = screen.height;
+	document.getElementById("page-content").style.height = ht+"px";
+	</script>
+	<!-- -->
+	<!------------------------ End of Page Content --------------------------------------------------->
+	
+	
+<?php include("footer.php"); ?>
