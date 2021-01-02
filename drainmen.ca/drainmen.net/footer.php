8d6aa87bd917e495822f841e2ff00e7a774308c9
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:34:34 -0800

    Create footer.php

---
+++ drainmen.ca/drainmen.net/footer.php
@@ -0,0 +1,51 @@
+	<!------------------------ Footer --------------------------------------------------->
+	
+	
+	<div id="footer">
+	<!-- <p align="center" style="padding-top: 20px"> Paintmen </p> -->
+	
+	<div id="footer-container">
+	
+	
+	<div id="footer-victoria">
+	<u><b>Paint Men (Victoria)</b></u><br>
+	3588 Quadra<br>
+	Victoria, BC<br>
+	V8W 3S3<br>
+	250-412-5698<br>
+	</div>
+
+	<div id="footer-vancouver">
+	<u><b>Paint Men (Vancouver)</b></u><br>
+	604-800-9801<br>
+	</div>
+	
+	<div id="footer-tollfree">
+	<u><b>Paint Men (Toll-free)</b></u><br> 
+	+1-888-383-4242<br>
+	</div>
+
+	<div id="footer-social">
+	<u><b>Social Media</b></u><br> 
+	<a href="https://www.facebook.com/PaintMenRenovationContractors">Check us out on facebook!</a><br>
+	</div>
+	
+	</div> <!-- footer-container -->
+
+
+	<div id="copyright">Copyright © 2016 Paint Men Global Services.</div>
+	
+	</div> <!-- footer -->
+	
+	
+	<!------------------------ End of Footer --------------------------------------------------->
+	
+	
+	
+	
+	
+	
+	
+	
+</body>
+</html>
