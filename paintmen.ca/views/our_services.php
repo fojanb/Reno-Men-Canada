28d8ab30af6301acb1f5e39d94f99bcbe737bbe3
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:38:57 -0800

    Create our_services.php

---
+++ paintmen.ca/views/our_services.php
@@ -0,0 +1,59 @@
+
+
+<div class="row">
+	<div class="container">
+		<div class="sectionHeader">
+			<div id="ourServices" class="mobile"></div>
+			<h1 id="ourServicesHeader"> Our Services </h1>
+		</div>
+		
+		<div class="col-md-10 col-md-offset-1 col-xs-12 postHeader">
+			<div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+				<div>
+					<div id="ourServices" class="desktop"></div>
+				</div>
+				<h3 class="desktop"> Services </h3>
+	            <ul>
+	             	<li>Heritage Home Specialists</li>
+                    <li>Wallpaper Specialists</li>
+                    <li>Roof Painting (Extend Your Roof Life)</li>
+                    <li>Interior & Exterior </li>
+                    <li>Painting & Staining </li>
+					<li>Wood, Drywall, Concrete, & Metal</li>
+					<li>Textured Wall & Ceiling, Repairs</li>
+					<li>'Popcorn' Ceiling Removal</li>
+					<li>General Repairs & Maintenance</li>
+					<li>Wallpapering & Removal</li>
+					<li>Caulking, Filling, Sealing, & Sanding</li>
+					<li>Cleaning & Detailing</li>
+					<li>Special Concrete Coatings & Epoxy</li>
+					<li>Plaster & Drywall  Repairs</li>
+					<li>Garage Floors</li>
+					<li>Graffiti Removal</li>
+					<li>Damp Proofing & Coatings</li>
+					<li>Waterproofing</li>
+					<li>Decorating & Design Consulting</li>
+					<li>Carpentry</li>
+	            </ul>
+	        </div>
+	        <div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3 desktop">
+	        	<div>
+					<div id="ourClients" class="desktop"></div>
+				</div>
+	        	<h4> Clients </h4>
+	        	<ul>
+	                <li>Residential</li>
+					<li>Commercial</li>
+					<li>Industrial</li>
+					<li>Marine</li>
+					<li>Strata Complexes</li>
+					<li>Condo Buildings</li>
+					<li>Apartment Buildings</li>
+					<li>Property Managers</li>
+					<li>Government</li>	
+					<li>Anyone <i class="fa fa-smile-o" aria-hidden="true"></i></li>
+	            </ul>
+	        </div>
+	    </div>
+	</div>
+</div>
