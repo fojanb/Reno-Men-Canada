492773b41b4388e438fe2b1994d7d340999c7d9a
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:37:06 -0800

    Create our_work.php

---
+++ drainmen.ca/views/our_work.php
@@ -0,0 +1,32 @@
+
+<div class="row">
+        <div class="col-xs-12">
+            <div class="container">
+                <h2 class="alt"> Our Work </h2>
+                <!-- Gallery  -->
+					<div class="gallery">
+						<article class="from-left">
+							<a href="resources/images/fulls/house1_before.jpg" class="image fit"><img src="resources/images/thumbs/house1_before.jpg" title="Before" alt="" /></a>
+						</article>
+						<article class="from-right">
+							<a href="resources/images/fulls/house1_after.jpg" class="image fit"><img src="resources/images/thumbs/house1_after.jpg" title="After" alt="" /></a>
+						</article>
+						<article class="from-left">
+							<a href="resources/images/fulls/wall1_before.jpg" class="image fit"><img src="resources/images/thumbs/wall1_before.jpg" title="Before" alt="" /></a>
+						</article>
+						<article class="from-right">
+							<a href="resources/images/fulls/wall1_after.jpg" class="image fit"><img src="resources/images/thumbs/wall1_after.jpg" title="After" alt="" /></a>
+						</article>
+						<article class="from-left">
+							<a href="resources/images/fulls/wall2_before.jpg" class="image fit"><img src="resources/images/thumbs/wall2_before.jpg" title="Before" alt="" /></a>
+						</article>
+						<article class="from-right">
+							<a href="resources/images/fulls/wall2_after.jpg" class="image fit"><img src="resources/images/thumbs/wall2_after.jpg" title="After" alt="" /></a>
+						</article>
+						<article class="from-left">
+							<a href="resources/images/fulls/house2_after.jpg" class="image fit"><img src="resources/images/thumbs/house2_after.jpg" title="After" alt="" /></a>
+						</article>
+					</div>
+            </div>
+        </div>
+    </div>
