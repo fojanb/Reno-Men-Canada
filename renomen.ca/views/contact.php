941dc6f977020a2c08181b5cbf11c031e57ca232
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:40:49 -0800

    Create contact.php

---
+++ renomen.ca/views/contact.php
@@ -0,0 +1,57 @@
+
+<div class="row">
+	<div class="container">
+		<div class="sectionHeader">
+			<div id="callGirl" class="mobile"></div>
+			<h1 id="contactHeader"> Contact Us </h1>
+		</div>
+
+		<div class="postHeader">
+			<div id="callGirl" class="desktop"></div>
+			<h3 class="info-panel-title">Call Us Now For a Free Quotation</h3>
+		</div>
+		<div class="col-xs-12">
+			<p>We proudly serve <b>Victoria</b>, the Capital Region (<b>CRD</b>), <b>Vancouver</b> and the lower mainland (<b>GVRD</b>).</p>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 col-xs-12 firstCol">
+				<span><u><b>Victoria</b></u></span><br>
+				<span>778-400-5474</span><br>		
+				<span>3588 Quadra Street</span><br>
+				<span>Victoria, BC,  V8X 1H2</span><br>
+			</div>
+			<div class="col-sm-4 col-xs-12 secondCol">
+				<span><u><b>Vancouver</b></u></span><br>
+				<span>604-757-9694</span><br>
+			</div>
+			<div class="col-sm-4 col-xs-12 thirdCol">
+				<span><u><b>Toll Free</b></u></span><br>
+				<span>+1-855-241-6588</span><br>
+			</div>
+		</div>
+		<div class="row addMarginBottom">
+			<div class="col-xs-12">
+				<h3 class="larger"> Affiliate Sites </h3>
+				<div class="col-xs-12">
+					<a href="http://www.paintmen.ca/">paintmen.ca</a>
+				</div>
+				<div class="col-xs-12">
+					<a href="http://drainmen.ca/">drainmen.ca</a>
+				</div>
+				<div class="col-xs-12">
+					<a href="http://solarmen.ca/">solarmen.ca</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<h3 class="larger"> Keep In Touch </h3>
+				<div class="col-xs-12">
+					<a href="https://www.facebook.com/RenoMenCanada" id="facebook" class="skel-layers-ignoreHref"><span class="icon fa fa-facebook fa-2x"></span></a>
+					<a href="#" id="google-plus" class="skel-layers-ignoreHref"><span class="icon fa fa-google-plus fa-2x"></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
