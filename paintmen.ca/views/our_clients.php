2c58aac89e774970609f32ee580d90c12074c639
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:39:02 -0800

    Create our_clients.php

---
+++ paintmen.ca/views/our_clients.php
@@ -0,0 +1,31 @@
+
+
+<div class="row">
+	<div class="container">
+		<div id="sectionHeader">
+			<div id="ourClients" class="mobile"></div>
+			<h1 id="ourServicesHeader"> Our Clients </h1>
+		</div>
+
+		<div class="col-md-10 col-md-offset-1 col-xs-12">
+			<div class="postHeader" style="text-align: center">
+				<div id="ourClients" class="desktop"></div>
+			</div>
+	        <div class="col-sm-4 col-sm-offset-5 col-xs-10 col-xs-offset-3">
+	            <ul>
+					<li>Residential</li>
+					<li>Commercial</li>
+					<li>Industrial</li>
+					<li>Marine</li>
+					<li>Strata Complexes</li>
+					<li>Condo Buildings</li>
+					<li>Apartment Buildings</li>
+					<li>Property Managers</li>
+					<li>Government</li>	
+					<li>Anyone <i class="fa fa-smile-o" aria-hidden="true"></i></li>
+	            </ul>
+	            </ul>
+	        </div>
+	    </div>
+	</div>
+</div>
