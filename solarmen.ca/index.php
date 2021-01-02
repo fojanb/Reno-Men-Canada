324d244c95004046ea22ae5f3024cce7bf66cc2f
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:41:20 -0800

    Create index.php

---
+++ solarmen.ca/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+
+
+?>
+
+<html>
+<head>
+	<link href="http://renomen.ca/solarmen.ca/css/solarmen.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="splash" style="margin-top: 40px" >
+		<h1>Solar Men Canada</h1>
+		<h2>Coming in 2017</h2>
+		<h2>SOLAR PANEL SERVICES FOR THE GREATER VICTORIA AND VANCOUVER AREAS</h2>
+	</div>
+</body>
+</html>
