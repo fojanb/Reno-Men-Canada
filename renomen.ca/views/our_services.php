e60a6ef25d0d6f49101920fa475a1e09393382e0
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:41:04 -0800

    Create our_services.php

---
+++ renomen.ca/views/our_services.php
@@ -0,0 +1,89 @@
+
+
+<div class="row">
+	<div class="container">
+		<div class="sectionHeader">
+			<div id="ourServices" class="mobile"></div>
+			<h1 id="ourServicesHeader"> Our Services </h1>
+		</div>
+		
+		<div class="col-md-10 col-md-offset-1 col-xs-12 postHeader">
+			<div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+				<div>
+					<div id="ourServices" class="desktop"></div>
+				</div>
+	        	<h4> Interior </h4>
+	        	<ul>
+	             	<li>Drywall Installation, Mud, Tape & Sealing</li>
+                    <li>Plaster, Drywall & Concrete Repairs</li>
+                    <li>Steel Stud & Acoustical Ceilings</li>
+                    <li>Finishing Carpentry, Wainscoting, Crown Moldings & Cabinetry</li>
+                    <li>Cabinetry Repairs</li>
+                    <li>Kitchen & Bathroom Remodelling</li>
+                    <li>Tile Repairs and New Installations</li>
+                    <li>Flooring Repairs and New Installations</li>
+                    <li>Leak Repairs and Plumbing Services</li>
+                    <li>Lighting Repairs and Installations</li>
+                    <li>Electrical Repairs and Installations</li>
+                    <li>Ductwork & Ventilation</li>
+                    <li>New Appliance Installations & Repairs</li>
+                    <li>Demolition, Removal, Recycling, Hauling and Disposal</li>
+	            </ul>
+	        </div>
+	        <div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+	        	<div>
+					<div id="ourClients" class="desktop"></div>
+				</div>
+	        	<h4> Exterior </h4>
+	        	<ul>
+	             	<li>Landscaping</li>
+	             	<li>Decks & Fences</li>
+	             	<li>Vinyl Decking Repairs and Installations</li>
+	             	<li>Foundations & Repairs</li>
+	             	<li>Structural Repairs</li>
+	             	<li>Insulation, Weather Stripping, Door Sweeps</li>
+	             	<li>Cedar Siding & Shingles, Concrete Plank</li>
+	             	<li>Door & Window Repairs and Installation</li>
+	             	<li>Metal Fabrication and Repairs</li>
+	             	<li>Caulking, Sealing, and Waterproofing</li>
+	             	<li>Damp Proofing and Coatings</li>
+	             	<li>Asphalt Repairs and Sealing</li>
+	             	<li>Gutter and Downspout Cleaning and Repairs</li>
+	             	<li>Roof Painting</li>
+	             	<li>Roof Cleaning, Moss and Algae Removal</li>
+	             	<li>Roofing and Repairs</li>
+	            </ul>
+	        </div>
+
+			<div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+	        	<h4> General </h4>
+	        	<ul>
+	             	<li>Heritage House Specialists</li>
+	             	<li>General Contracting</li>
+	             	<li>Maintenance</li>
+	             	<li>Decorating and Design Consulting</li>
+	             	<li>General Repairs</li>
+	             	<li>Remodelling and Renovations</li>
+	             	<li>Cleaning and Pressure Washing</li>
+	            </ul>
+	        </div>
+	        
+	        <div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3 desktop">
+	            <h4> Our Clients </h4>
+	            <ul>
+	                <li>Residential</li>
+					<li>Commercial</li>
+					<li>Strata Complexes</li>
+					<li>Condo Buildings</li>
+					<li>Landlords</li>
+					<li>Apartment Buildings</li>
+					<li>Property Managers</li>
+					<li>Government</li>	
+					<li>Anyone <i class="fa fa-smile-o" aria-hidden="true"></i></li>
+	            </ul>
+	        </div>
+
+
+	    </div>
+	</div>
+</div>
