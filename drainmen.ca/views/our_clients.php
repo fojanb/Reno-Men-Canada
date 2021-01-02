ee6dc72ea04f5698f9705e61db63f8a4fb8fe3a9
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:37:02 -0800

    Create our_clients.php

---
+++ drainmen.ca/views/our_clients.php
@@ -0,0 +1,29 @@
+
+
+<div class="row">
+	<div class="container">
+		<div id="sectionHeader">
+			<div id="ourClients" class="mobile"></div>
+			<h1 id="ourServicesHeader"> Our Clients </h1>
+		</div>
+
+		<div class="col-md-10 col-md-offset-1 col-xs-12">
+			<div class="postHeader" style="text-align: center">
+				<div id="ourClients" class="desktop"></div>
+			</div>
+	        <div class="col-sm-4 col-sm-offset-5 col-xs-10 col-xs-offset-3">
+	            <ul>
+	                <li>Residential</li>
+					<li>Commercial</li>
+					<li>Strata Complexes</li>
+					<li>Condo Buildings</li>
+					<li>Landlords</li>
+					<li>Apartment Buildings</li>
+					<li>Property Managers</li>
+					<li>Government</li>	
+					<li>Anyone <i class="fa fa-smile-o" aria-hidden="true"></i></li>
+	            </ul>
+	        </div>
+	    </div>
+	</div>
+</div>
