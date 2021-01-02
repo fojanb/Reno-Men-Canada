78f8c5cc7b7ccc06e5392208ee22361f3e7bc8b8
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:33:26 -0800

    Create about_us.php

---
+++ drainmen.ca/drainmen.net/about_us.php
@@ -0,0 +1,99 @@
+<?php include("header.php"); ?>
+
+	<!------------------------ Page Content --------------------------------------------------->
+	
+	
+	<!--
+	<div id="page-content-header">
+	  <section class="header" id="page-header-title">
+	    <div class="container">
+		<h5 class="page-title">What's your favourite colour ?</h1>
+	    </div>
+	  </section>
+	</div>
+	-->
+	
+	
+	<div id="page-content" class="about_us-background" style="padding-bottom:20px;">
+	<div id="page-content-margin"><br/></div>	
+	
+	
+	<div class="panel panel-main-content">
+	  <div class="panel-heading"><h3 class="panel-title">Our Services</h3></div>
+	  <div class="panel-body">
+	  	<div class="row">
+		<div class="our-services-left"><p>At Paint Men we are ready to help with any Interior Remodeling and Renovation project, for both Residential and Commercial Clients; from a simple painting job to larger decorating and renovating projects.   Our expert staff will make your dreams a reality.</p>
+		</div>
+		<div class="our-services-right">
+		  <!--
+		  <div><button type="button" class="btn btn-primary" style="margin-bottom: 10px; width:80px;">Interiors</button></div>
+		  <div><button type="button" class="btn btn-primary" style="margin-bottom: 10px; width:80px;">Exteriors</button></div>
+		  -->
+		</div>
+        	</div>
+	  </div>
+	</div>
+	
+	
+	<div class="panel panel-main-content">
+	  <div class="panel-heading"><h3 class="panel-title">Residential Work</h3></div>
+	  <div class="panel-body">
+	  	<div class="row">
+		<div class="our-services-left"><p>At Paint Men we are ready to help with any Interior Remodeling and Renovation project, for both Residential and Commercial Clients; from a simple painting job to larger decorating and renovating projects.   Our expert staff will make your dreams a reality.</p>
+		</div>
+        	</div> <!-- end row -->
+	  </div>
+	</div>	
+	
+	<div class="panel panel-main-content">
+	  <div class="panel-heading"><h3 class="panel-title">Commercial Projects</h3></div>
+	  <div class="panel-body">
+	  	<div class="row">
+		<div class="our-services-left"><p>At Paint Men we are ready to help with any Interior Remodeling and Renovation project, for both Residential and Commercial Clients; from a simple painting job to larger decorating and renovating projects.   Our expert staff will make your dreams a reality.</p>
+		</div>
+        	</div> <!-- end row -->
+	  </div>
+	</div>	
+	
+	<div class="panel panel-main-content">
+	  <div class="panel-heading"><h3 class="panel-title">What we do</h3></div>
+	  <div class="panel-body">
+	    <div class="row">
+	    <div class="our-services-left">
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Painting and Staining to all Paint Grade Surfaces ... Wood, Drywall, Concrete, Metal, etc...</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Textured Wall and Ceiling, Repairs and Remodeling, "Popcorn" Ceiling Removal</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Wall Paper - Hanging and Removal</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>General Repairs, Prep Work, Caulking, Filling, Sanding, Cleaning and Detailing</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Special Concrete Coatings & Epoxy, Garage Floors, Concrete Staining</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Drywall Installation, Mud, Tape and Sealing</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Plaster, Drywall and Concrete Repairs</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Steel Stud and Acoustical Ceilings</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Carpentry and Structural Repairs</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Finishing Carpentry, Wainscoting, Crown Moldings & Cabinetry - New Installations and Repairs</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Decorating and Design Consulting</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Caulking, Sealing and Waterproofing</div>
+	    	<div style="padding-bottom:2px;"><span class="glyphicon glyphicon-ok" aria-hidden="true" style="padding-right:10px"></span>Damp Proofing and Coatings</div>
+	    </div>
+	    <div class="our-services-right">
+	    </div>
+	    </div> <!-- end of row -->
+	  </div>
+	</div> <!-- end of panel -->
+	
+	
+	</div> <!-- end page-content -->
+	
+	<!--
+	<script language="javascript">
+	var ht = screen.height;
+	document.getElementById("page-content").style.height = ht+"px";
+	</script>
+	-->
+	
+	
+
+	<!------------------------ End of Page Content --------------------------------------------------->
+	
+
+
+<?php include("footer.php"); ?>
