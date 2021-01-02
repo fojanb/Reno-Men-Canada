8252decd0a2a2eaa7613aab456a01f2901f949f3
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:39:10 -0800

    Create about.php

---
+++ paintmen.ca/views/about.php
@@ -0,0 +1,112 @@
+<div>
+    <div class="row">
+        <div class="col-xs-12">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+                <li data-target="#myCarousel" data-slide-to="5"></li>
+                <li data-target="#myCarousel" data-slide-to="6"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="resources/images/carousel1.jpg" />
+                     <div class="carousel-caption">
+                        <h3 id="before">Before</h3>
+                        <h3 id="after">After</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel2.jpg" />
+                    <div class="carousel-caption">
+                        <h3 id="before"></h3>
+                        <h3 id="after"></h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel3.jpg" />
+                    <div class="carousel-caption">
+                        <h3 id="before">Before</h3>
+                        <h3 id="after">After</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel4.jpg" />
+                    <div class="carousel-caption">
+                        <h3 id="before">Before</h3>
+                        <h3 id="after">After</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel5.jpg" />
+                    <div class="carousel-caption">
+                        <h3 id="before"></h3>
+                        <h3 id="after"></h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel6.jpg" />
+                    <div class="carousel-caption">
+                        <h3 id="before"></h3>
+                        <h3 id="after"></h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel7.jpg" />
+                    <div class="carousel-caption">
+                        <h3 id="before">Before</h3>
+                        <h3 id="after">After</h3>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="aboutUs">
+        <div class="containter">
+            <h1 id="aboutUsHeader"> About Us</h1>
+            <div class="col-md-10 col-md-offset-1 col-xs-12 postHeader">
+                <div class="centerContent">
+                    <p> At Paint Men we are ready to help with any new build, remodeling and renovation project, for both residential and commercial clients; from a simple painting job to larger decorating and renovating projects. Our expert staff will make your dreams a reality.</p>
+
+                    <h3 style="text-align: center"> Why Us? </h3>
+                </div>
+                <div id="firstCol" class="col-sm-4 col-sm-offset-2 col-xs-9 col-xs-offset-3">
+                    <ul class="noMargin">
+                        <li>Heritage Home Specialists</li>
+                        <li>Wallpaper Specialists</li>
+                        <li>Roof Painting (Extend Your Roof Life)</li>
+                        <li>Expert Friendly Staff</li>
+                        <li>Green Practices</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+                    <ul>
+                        <li>Free Colour Consultation</li>
+                        <li>Accurate Quotes</li>
+                        <li>Best Quality Products</li>
+                        <li>Well Equipped</li>
+                        <li>Clean Job Sites</li>
+                        <li>Dependable & Reliable</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
