a16ce679356d9537f465037049d536a2d421baff
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:36:57 -0800

    Create about.php

---
+++ drainmen.ca/views/about.php
@@ -0,0 +1,85 @@
+<div>
+    <div class="row">
+        <div class="col-xs-12">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="resources/images/carousel1.jpg" />
+                     <div class="carousel-caption">
+                        <h3>Pressure Washing</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel2.jpg" />
+                    <div class="carousel-caption">
+                        <h3>Flooded Basement Repairs</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel3.jpg" />
+                    <div class="carousel-caption">
+                        <h3>Drain Scoping & Cleaning</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="resources/images/carousel4.jpg" />
+                    <div class="carousel-caption">
+                        <h3>Mini Excavating & Trucking</h3>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="aboutUs">
+        <div class="containter">
+            <h1 id="aboutUsHeader"> About Us</h1>
+            <div class="col-md-10 col-md-offset-1 col-xs-12 postHeader">
+                <div class="centerContent">
+                    <p> At Drain Men we are ready to help with any new build, remodeling and renovation project, for both residential and commercial clients; from tree root removal to excavation and installation of a new perimeter drainage system. We are experts at foundation repairs and water proofing to keep your basement dry. Call us now for a free quotation and helpful advice. </p>
+
+                    <h3 style="text-align: center"> Why Us? </h3>
+                </div>
+                <div id="firstCol" class="col-sm-4 col-sm-offset-2 col-xs-9 col-xs-offset-3">
+                    <ul class="noMargin">
+                        <li>Expert Friendly Staff </li>
+                        <li>Green Practices</li>
+                        <li>Best Quality Products</li>
+                        <li>Realistic Timelines</li>
+                        <li>Insured</li>
+                        <li>Fast Response</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+                    <ul>
+                        <li>Accurate Quotes</li>
+                        <li>Well Equipped</li>
+                        <li>Organized</li>
+                        <li>Clean Job Sites</li>
+                        <li>Dependable and Reliable</li>
+                        <li>Community Minded</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
