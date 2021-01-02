9d9310dbd25b448bc3548509f0609f7c16844d71
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 15:37:08 -0800

    Create our_services.php

---
+++ drainmen.ca/views/our_services.php
@@ -0,0 +1,53 @@
+
+
+<div class="row">
+	<div class="container">
+		<div class="sectionHeader">
+			<div id="ourServices" class="mobile"></div>
+			<h1 id="ourServicesHeader"> Our Services </h1>
+		</div>
+
+		<div class="col-md-10 col-md-offset-1 col-xs-12 postHeader">
+			<div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3">
+				<div>
+					<div id="ourServices" class="desktop"></div>
+				</div>
+	        	<h4 class="desktop"> Services </h4>
+	        	<ul>
+	             	<li>Perimeter Drains / Draintile Replacement</li>
+                    <li>Drain Scoping / Video Inspection</li>
+                    <li>Drain Cleaning & Flushing</li>
+                    <li>Power Rooting</li>
+                    <li>High Pressure Water Jetting</li>
+                    <li>Tree Roots Removal</li>
+                    <li>Inspections</li>
+                    <li>Utility and Pipe Locating</li>
+                    <li>Water Line Service, Replacement & Upgrades</li>
+                    <li>Sewer and Storm Lateral Replacements</li>
+                    <li>Basement & Foundation Water Proofing</li>
+                    <li>Catch Basin Installation & Maintenance</li>
+                    <li>Concrete and Asphalt Removal and Replacement</li>
+                    <li>Pump - Repair & Replacement</li>
+                    <li>Mini Excavating & Trucking</li>
+	            </ul>
+	        </div>
+	        <div class="col-sm-5 col-sm-offset-1 col-xs-9 col-xs-offset-3 desktop">
+	        	<div>
+					<div id="ourClients" class="desktop"></div>
+				</div>
+	        	<h4> Clients </h4>
+	        	<ul>
+	                <li>Residential</li>
+					<li>Commercial</li>
+					<li>Strata Complexes</li>
+					<li>Condo Buildings</li>
+					<li>Landlords</li>
+					<li>Apartment Buildings</li>
+					<li>Property Managers</li>
+					<li>Government</li>
+					<li>Anyone <i class="fa fa-smile-o" aria-hidden="true"></i></li>
+	            </ul>
+	        </div>
+	    </div>
+	</div>
+</div>
